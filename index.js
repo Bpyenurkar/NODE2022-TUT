1adbc775f056fc051e8ebb154f058e3c27dd669c
Bpyenurkar <104269996+Bpyenurkar@users.noreply.github.com>
2023-04-30 15:13:07 +0530

    add

---
+++ index.js
@@ -0,0 +1,60 @@
+// var a=20;
+// var b=30;
+// var c=40;
+// console.warn(a+b+c);
+
+//conditional sentence
+
+// var x='20';
+// var x=20;
+// if(x == 20){
+//     console.log("matched");
+// }
+
+// var x='20';
+// for(i=0;i<20;i++){
+//     console.log(i);
+// }
+
+// const arr=[2,4,6,7,8,9];
+// console.log(arr[0]);
+
+// const app = require('./app')
+// const arr=[2,4,7,1,3,8,3];
+// let result = arr.filter((item)=>{
+// return item === 3;
+// return item >=4;
+// })
+// console.log(result);
+
+//    core Modules
+
+// console.log("Bhushan");
+// const fs =require('fs');
+// fs.writeFileSync("hello text","Code step by step");
+
+// const fs = require('fs').writeFileSync;
+// fs("abc.txt","abc");
+
+
+// core module-http
+ 
+// const http =require('http');
+// http.createServer((req,resp)=>{
+//     req.write("<h1>I am, BHushan</h1>")
+//     resp.end();
+// }).listen(4500);
+
+// const dataControl=(req,resp)=>{
+
+//     req.write("<h1>I am, BHushan</h1>")
+//     resp.end();
+// }
+// http.createServer(dataControl).listen(4500);
+
+// http.createServer((req,resp)=>{
+
+//        req.write("<h1>I am, BHushan</h1>")
+//          resp.end();
+//      }).listen(4500);
+
